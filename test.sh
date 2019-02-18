03e9eff45d7ed95a04a85539351af35fde017136
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-02-18 17:11:19 -0500

    adding nyc, istambul etc, and test folder

---
+++ test.sh
@@ -0,0 +1,1 @@
+NODE_ENV=test ./node_modules/nyc/bin/nyc.js  --reporter=lcov --reporter=html ./node_modules/mocha/bin/mocha --exit --require @babel/register --require @babel/polyfill --timeout 10000 $option "test/**/*.js";
